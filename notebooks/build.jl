5540b9c768ccc9bc4aa977b848f0e432377e4b8d
Iain Dunning <iaindunning@gmail.com>
2015-05-16 16:08:59 -0400

    Links to examples

--- notebooks/build.jl
+++ notebooks/build.jl
@@ -8,11 +8,33 @@ cd("source/notebooks") do
     run(`ipython nbconvert --to html --template basic $file`)
   end
 end
+nbs = Any[]
 for file in readdir("source/notebooks")
   !contains(file,".html") && continue
   println(file)
   middle = readall("source/notebooks/"*file)
   fp = open(file,"w")
+  push!(nbs, file[1:end-5])
   println(fp, header, middle, footer)
   close(fp)
 end
+
+fp = open("index.html","w")
+println(fp, header)
+
+println(fp, """
+  <div class="row">
+    <div class="col-md-12">
+      <h1>JuliaOpt Example Notebooks</h1>
+      <ul>
+""")
+for nb in nbs
+  println(fp, "<li><a href=\"$(nb*".html")\">$(nb)</a></li>")
+end
+println(fp, """
+      </ul>
+    </div>
+  </div>
+""")
+println(fp, footer)
+close(fp)
